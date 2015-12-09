2dbbbdd762724417ea675fff4f3c4cbbdae80fb8
Vlad <vlad@Vlads-MacBook-Pro.local>
2015-12-09 20:20:48 +1100

    Initial commit of clean new project and .gitignore file. Created with Xcode 7.1.1 using Single View template.

---
+++ ATask/ATask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATask
+//
+//  Created by Vlad on 9/12/2015.
+//  Copyright © 2015 Vlad Paraschiv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
